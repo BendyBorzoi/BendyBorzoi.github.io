f027ba24c5dcec51b906f4a440cdb5e1dd8a5e06
Jonas <jonasstenger@outlook.com>
2018-07-20 13:44:49 +0200

    rip nav

--- nav.js
+++
@@ -1,9 +0,0 @@
-let nav = document.getElementById('nav');
-xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState === 4) {
-        nav.innerHTML = this.responseText;
-    }
-};
-xhttp.open("GET", 'nav.html', true);
-xhttp.send();
